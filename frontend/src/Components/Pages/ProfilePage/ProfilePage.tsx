3a97a11003a64cced566e608c5eca261bb3eeb2d
Renakezo <tchernosckutoff.egor@yandex.ru>
2026-05-30 02:43:40 +0500

    add toogle theme button

--- frontend/src/Components/Pages/ProfilePage/ProfilePage.tsx
+++ frontend/src/Components/Pages/ProfilePage/ProfilePage.tsx
@@ -1,12 +1,21 @@
+import CalendarIcon from '@/Assets/calendar.svg?react'
+import EmailIcon from '@/Assets/email.svg?react'
+import LogoutIcon from '@/Assets/logout.svg?react'
+import MoonIcon from '@/Assets/moon.svg?react'
+import ProfileIcon from '@/Assets/profile.svg?react'
+import SunIcon from '@/Assets/sun.svg?react'
 import Button from '@/Components/UI/Button'
 import Card from '@/Components/UI/Card'
 import { useAuth } from '@/Contexts/AuthContext'
+import { useTheme } from '@/Contexts/ThemeContext'
 import { useTitle } from '@/Hooks'
 import { useNavigate } from 'react-router-dom'
 import styles from './ProfilePage.module.scss'
 
 export const ProfilePage = () => {
 	useTitle('Профиль')
+
+	const { theme, toggleTheme } = useTheme()
 	const { user, logout } = useAuth()
 	const navigate = useNavigate()
 
@@ -29,21 +38,21 @@ export const ProfilePage = () => {
 				<p>Данные вашего профиля</p>
 				<div className={styles.info_group}>
 					<Card className={styles.info_item}>
-						<img src='/profile.svg' alt='Пользователь' />
+						<ProfileIcon />
 						<div className={styles.info_text}>
 							<p className={styles.info_label}>Имя пользователя</p>
 							<p className={styles.info_value}>{user?.name}</p>
 						</div>
 					</Card>
 					<Card className={styles.info_item}>
-						<img src='/email.svg' alt='Почта' />
+						<EmailIcon />
 						<div className={styles.info_text}>
 							<p className={styles.info_label}>Email</p>
 							<p className={styles.info_value}>{user?.email}</p>
 						</div>
 					</Card>
 					<Card className={styles.info_item}>
-						<img src='/calendar.svg' alt='Календарь' />
+						<CalendarIcon />
 						<div className={styles.info_text}>
 							<p className={styles.info_label}>Дата регистрации</p>
 							<p className={styles.info_value}>
@@ -56,16 +65,27 @@ export const ProfilePage = () => {
 
 			<Card className={styles.actions_card}>
 				<h3>Действия</h3>
-				<Button
-					type='warning'
-					onClick={() => {
-						logout()
-						navigate('/')
-					}}
-				>
-					<img src='/logout.svg' alt='Выход' />
-					<p>Выйти из аккаунта</p>
-				</Button>
+				<div className={styles.buttons}>
+					<Button
+						type={`${theme}`}
+						onClick={() => {
+							toggleTheme()
+						}}
+					>
+						{theme === 'dark' ? <MoonIcon /> : <SunIcon />}
+						<p>{theme === 'dark' ? 'Темная' : 'Светлая'} тема</p>
+					</Button>
+					<Button
+						type='warning'
+						onClick={() => {
+							logout()
+							navigate('/')
+						}}
+					>
+						<LogoutIcon />
+						<p>Выйти из аккаунта</p>
+					</Button>
+				</div>
 			</Card>
 		</div>
 	)
